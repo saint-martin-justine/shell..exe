cbde262db12fadf83ea9e69d5ac7632ab0a00e58
saint-martin-justine <justine.saint-martin@laplateforme.io>
2022-09-28 18:53:58 +0200

    envoyer fichier 1 dans fichier 2

---
+++ job6/my_calculator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $2 == "x" ]];
+then
+	multiplication=$(($1*$3))
+	echo "$multiplication"
+
+elif [[ $2 == "+" ]];
+then
+	addition=$(($1+$3))
+	echo "$addition"
+
+elif [[ $2 == "-" ]];
+then
+	soustraction=$(($1-$3))
+	echo "$soustraction"
+
+elif [[ $2 == "/" ]];
+then
+	division=$(($1/$3))
+	echo "$division"
+
+else
+	echo "Mauvais argument"
+fi
